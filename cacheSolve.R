0dea58712983949ecc36f96cadebf5cf422fd665
ktleung <kelvin619@gmail.com>
2016-10-02 16:46:35 -0700

    modify makeCacheMatrix.R and cacheSolve.R for finding inverse of a square inreversible matrix

--- cacheSolve.R
+++ cacheSolve.R
@@ -1,13 +1,24 @@
-cachemean <- function(x, ...) {
-  m <- x$getmean()
+cacheSolve<- function(x, ...) {
+  # Input: x = output of makeCacheMatrix()
+  # Output: return inverse of the original matrix, i.e. input to makeCacheMatrix()
+  #
+  
+  # getting inverse of the matrix
+  m <- x$getInv()
+  # check if inverse has been computed 
   if(!is.null(m)) {
     message("getting cached data")
     return(m)
   }
+  
+  # if not compute: compute inverse 
   data <- x$get()
-  m <- mean(data, ...)
-  x$setmean(m)
-  m
+  # compute inverse of data 
+  m <- solve(data, ...)
+  
+  # set inverse of matrix in the cache via the setInv function
+  x$setInv(m)
+  
+  # return inverse
+  return(m)
 }
-
-
--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -1,28 +1,35 @@
-makeCacheMatrix() <- function(x = matrix()) {
-  # Input: a squared inversible matrix
-  # Output: a list containing the followings
-  #             1. set the value of the matrix
-  #             2. get the value of the matrix
-  #             3. set the value of the mean
-  #             4. get the value of the mean
-  #
-  mat <- NULL
-  set <- function(y) {
-    # '<<-' 
-    # cause a search to be made through parent 
-    # environments for an existing definition 
-    # of the variable being assigned. If such 
-    # a variable is found (and its binding is 
-    # not locked) then its value is redefined, 
-    # otherwise assignment takes place in the 
-    # global environment.
+makeCacheMatrix <- function(x = matrix()) {
+  ## Input: x, a square invertible matrix
+  ## Output: a list containing the following functions to 
+  ##          1. set the value of matrix
+  ##          2. get the value of matrix
+  ##          3. set the inverse of matrix
+  ##          4. get the inverse of matrix
+  ##  this will be used for cacheSolve() ...
+  ##
+  
+  m = NULL
+  
+  # set the value of matrix
+  set = function(y) {
+    # "<<-" causes a search to be made through parent environments for 
+    # an existing definition of the variable being assigned. If such 
+    # a variable is found (and its binding is not locked) then its value 
+    # is redefined, otherwise assignment takes place in the global 
+    # environment.
     x <<- y
-    mat <<- NULL
+    m <<- NULL
   }
-  get <- function() x
-  setInv <- function(invM) mat <<- invM
-  getInv <- function() mat
-  list(set = set, get = get,
-       setInv = setInv,
-       getInv = getInv)
+  
+  # get the value of matrix
+  get = function() x
+  
+  # set inverse of the matrix
+  setInv = function(m_inv) m <<- m_inv
+  
+  # get inverse of the matrix
+  getInv = function() m
+  
+  # return a list containing these functions 
+  list(set=set, get=get, setInv=setInv, getInv=getInv)
 }
